9b410cdba1c9389657177eb4c13c99c43202b482
Andrei Drazdouski <a.drzdvsk@gmail.com>
2024-07-17 08:44:57 +0300

    create searchGitTest

--- src/test/java/SearchGitTest.java
+++ src/test/java/SearchGitTest.java
@@ -3,19 +3,28 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SearchGitTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://github.com/";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
     }
 
     @Test
-    public void findSelenideTest() {
-        open();
+    public void searchGitTest() {
+        open("https://github.com/");
+        $(byTagAndText("button", "Solutions"))
+                .hover();
+        $$(".HeaderMenu-dropdown-link")
+                .findBy(href("https://github.com/enterprise"))
+                .shouldBe(visible)
+                .click();
+        $("#hero-section-brand-heading")
+                .shouldHave(text("The AI-powered developer platform."));
+
     }
 }
